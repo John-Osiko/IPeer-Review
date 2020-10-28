8fec7b90badf8e59971b0e19a370167a8684ae68
John-Osiko <johnmaxosiko@gmail.com>
2020-10-29 02:09:15 +0300

    Updated imports

--- peerapp/urls.py
+++ peerapp/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import re_path, path, include
+#from django.views.generic import RedirectView
 
-from .views import home, project_view, signup, profile, edit_profile, upload, index
+from .views import home, project_view, signup, profile, upload, index
 
 urlpatterns = [
     path('', home, name='home'),
@@ -10,8 +11,8 @@ urlpatterns = [
     path('signup/', signup, name='signup'),
     path('upload/', upload, name='upload'),
     path('profile/<username>/', profile, name='profile'),
-    path('profile/<username>/settings', edit_profile, name='edit'),
     path('account/', include('django.contrib.auth.urls')),
+    #path(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
